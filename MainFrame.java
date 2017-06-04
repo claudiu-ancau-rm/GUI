186c6799a501bdb8199a7853f1381160f06ec38c
claudiu-ancau-rm <claudiu.ancau@imobiliare.ro>
2017-06-05 01:44:51 +0300

    Add files via upload

--- MainFrame.java
+++ MainFrame.java
@@ -1,9 +1,11 @@
 import com.jogamp.opengl.*;
 import com.jogamp.opengl.awt.GLCanvas;
 import com.jogamp.opengl.fixedfunc.GLMatrixFunc;
+import com.jogamp.opengl.glu.GLU;
 import com.jogamp.opengl.util.Animator;
 
 import javax.swing.*;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -14,8 +16,13 @@ public class MainFrame
 
     private GLCanvas canvas;
     private Animator animator;
-    static final private String a = "";
-
+    private final int NO_TEXTURES = 1;
+    private int texture[] = new int[NO_TEXTURES];
+    TextureReader.Texture[] tex = new TextureReader.Texture[NO_TEXTURES];
+    private GLU glu;
+    String PRIMA_CULOARE;
+    String CULOAREA_DOI;
+    String CULOAREA_TREI;
     // For specifying the positions of the clipping planes (increase/decrease the distance) modify this variable.
     // It is used by the glOrtho method.
     private double v_size = 10.0;
@@ -69,9 +76,42 @@ public class MainFrame
     }
 
     public void init(GLAutoDrawable canvas) {
-        // Obtaining the GL instance associated with the canvas.
+        PRIMA_CULOARE = culoareRandom();
+        CULOAREA_DOI = culoareRandom();
+        CULOAREA_TREI = culoareRandom();
         GL2 gl = canvas.getGL().getGL2();
+        // Create a new GLU object.
+        glu = GLU.createGLU();
 
+        // Generate a name (id) for the texture.
+        // This is called once in init no matter how many textures we want to generate in the texture vector
+        gl.glGenTextures(NO_TEXTURES, texture, 0);
+
+        // Define the filters used when the texture is scaled.
+        gl.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MIN_FILTER, GL.GL_LINEAR);
+        gl.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MAG_FILTER, GL.GL_LINEAR);
+
+        // Do not forget to enable texturing.
+        gl.glEnable(GL.GL_TEXTURE_2D);
+
+        // The following lines are for creating ONE texture
+        // If you want TWO textures modify NO_TEXTURES=2 and copy-paste again the next lines of code
+        // up until (and including) this.makeRGBTexture(...)
+        // Modify texture[0] and tex[0] to texture[1] and tex[1] in the new code and that's it
+
+        // Bind (select) the texture.
+        gl.glBindTexture(GL.GL_TEXTURE_2D, texture[0]);
+
+        // Read the texture from the image.
+        try {
+            tex[0] = TextureReader.readTexture("a.PNG");
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        // Construct the texture and use mipmapping in the process.
+        this.makeRGBTexture(gl, glu, tex[0], GL.GL_TEXTURE_2D, true);
         // Setting the clear color -- the color which will be used to erase the canvas.
         gl.glClearColor(0, 0, 0, 0);
 
@@ -99,16 +139,19 @@ public class MainFrame
 
     }
 
+    //functia care creaza poligoanele pentru verificarea raspunsurilor corecte
     public void cordonateRaspunsuriGresite(float[] combinatieCuloare, float numarIncercare, float variatiaX) {
         float[] cordonate = new float[]{-8.8f + variatiaX, -8.5f + variatiaX, 7.8f - numarIncercare, 7.5f - numarIncercare};
         construirePoligon(combinatieCuloare, cordonate);
     }
 
+    //functia care creaza poligoanele pentru verificarea raspunsurilor corecte
     public void cordonateRaspunsuriCorecte(float[] combinatieCuloare, float numarIncercare, float variatiaX) {
         float[] cordonate = new float[]{-8.8f + variatiaX, -8.5f + variatiaX, 7.4f - numarIncercare, 7.2f - numarIncercare};
         construirePoligon(combinatieCuloare, cordonate);
     }
 
+    //functia care construieste un poligon si il coloreaza
     public void construirePoligon(float[] combinatieCuloare, float[] cordonate) {
         GL2 gl = canvas.getGL().getGL2();
         gl.glBegin(GL2.GL_POLYGON);
@@ -123,15 +166,18 @@ public class MainFrame
         gl.glEnd();
     }
 
+    //un strin cu numele celor 3 culori care trebuie ghicite
     public String[] culoareCorecta() {
-        String[] listaCuloriCorecte = new String[]{"rosu", "galben", "verde"};
+        String[] listaCuloriCorecte = new String[]{PRIMA_CULOARE, CULOAREA_DOI, CULOAREA_TREI};
         return listaCuloriCorecte;
     }
+
     //reprezinta culoarea aleasa, acesta functie va primi rezultatul de la eventul de dat click pe una din cele 6 culori
     public String culoareAleasa() {
 
-        return "alb";
+        return PRIMA_CULOARE;
     }
+
     //definesc culorile
     public float[] culoare(String culoare) {
         float[] combinatieCuloare;
@@ -156,6 +202,12 @@ public class MainFrame
             case "gri":
                 combinatieCuloare = new float[]{0.5f, 0.5f, 0.5f};
                 break;
+            case "orange":
+                combinatieCuloare = new float[]{1, 0.5f, 0};
+                break;
+            case "mov":
+                combinatieCuloare = new float[]{0, 0, 0.5f};
+                break;
             default:
                 combinatieCuloare = new float[]{0, 0, 0};
                 break;
@@ -163,6 +215,7 @@ public class MainFrame
 
         return combinatieCuloare;
     }
+
     //genereaza o culoare random, deocamdata nefunctional
     public String culoareRandom() {
         List<String> list = new ArrayList<String>();
@@ -170,7 +223,7 @@ public class MainFrame
         list.add("galben");
         list.add("verde");
         list.add("albastru");
-        list.add("alb");
+        list.add("orange");
         list.add("gri");
         Random randomizer = new Random();
         String random = list.get(randomizer.nextInt(list.size()));
@@ -180,21 +233,21 @@ public class MainFrame
     public void display(GLAutoDrawable canvas) {
         GL2 gl = canvas.getGL().getGL2();
         gl.glClear(GL.GL_COLOR_BUFFER_BIT);
-        //patratele principale unde sunt culorile pe care trebie sa le ghicim
 
-        construirePoligon(culoare(culoareCorecta()[0]), new float[]{1, 4.9f, 1, 5});
-        construirePoligon(culoare(culoareCorecta()[1]), new float[]{5, 8.9f, 5, 1});
-        construirePoligon(culoare(culoareCorecta()[2]), new float[]{0.9f, -3, 1, 5});
+        //patratele principale unde sunt culorile pe care trebie sa le ghicim
+        construirePoligon(culoare(culoareCorecta()[0]), new float[]{0.9f, -3, 1, 5});
+        construirePoligon(culoare(culoareCorecta()[1]), new float[]{1, 4.9f, 1, 5});
+        construirePoligon(culoare(culoareCorecta()[2]), new float[]{5, 8.9f, 5, 1});
+
 
         //zona unde sunt incercarile de pana acum
-
         construirePoligon(culoare("gri"), new float[]{-9, -7, 8, -7});
 
         //combinatia de culori incercata
         int i = 0;
         while (i < 15) {
             cordonateRaspunsuriGresite(culoare(culoareAleasa()), i, 0);
-            if ("rosu" == culoareCorecta()[0])
+            if (culoareAleasa() == culoareCorecta()[0])
                 cordonateRaspunsuriCorecte(culoare("verde"), i, 0);
             else
                 cordonateRaspunsuriCorecte(culoare("rosu"), i, 0);
@@ -214,17 +267,23 @@ public class MainFrame
         }
 
         //culorile din care trebuie sa alegem
-
         construirePoligon(culoare("rosu"), new float[]{-5, -2.1f, -8, -5});
         construirePoligon(culoare("verde"), new float[]{-2, 0.9f, -8, -5});
         construirePoligon(culoare("galben"), new float[]{1, 3.9f, -8, -5});
         construirePoligon(culoare("albastru"), new float[]{4, 6.9f, -8, -5});
-        construirePoligon(culoare("alb"), new float[]{7, 9.9f, -8, -5});
-        construirePoligon(culoare("gri"), new float[]{10, 12.9f, -8, -5});
+        construirePoligon(culoare("orange"), new float[]{7, 9.9f, -8, -5});
+        construirePoligon(culoare("mov"), new float[]{10, 12.9f, -8, -5});
 
 
     }
 
+    private void makeRGBTexture(GL gl, GLU glu, TextureReader.Texture img, int target, boolean mipmapped) {
+        if (mipmapped) {
+            glu.gluBuild2DMipmaps(target, GL.GL_RGB8, img.getWidth(), img.getHeight(), GL.GL_RGB, GL.GL_UNSIGNED_BYTE, img.getPixels());
+        } else {
+            gl.glTexImage2D(target, 0, GL.GL_RGB, img.getWidth(), img.getHeight(), 0, GL.GL_RGB, GL.GL_UNSIGNED_BYTE, img.getPixels());
+        }
+    }
 
     public void reshape(GLAutoDrawable canvas, int left, int top, int width, int height) {
         GL2 gl = canvas.getGL().getGL2();
